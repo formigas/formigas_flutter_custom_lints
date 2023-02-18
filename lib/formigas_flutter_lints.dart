d8df725fe2279a37cb4b2828ac2cecd66e04b075
Julian Zellner <julian@zellner.cc>
2023-02-18 18:10:36 +0100

    renaming

--- lib/formigas_flutter_lints.dart
+++ lib/formigas_flutter_lints.dart
@@ -1,6 +1,7 @@
 import 'package:custom_lint_builder/custom_lint_builder.dart';
 import 'package:formigas_flutter_lints/lints/sorting/sort_constructor_parameters_alphabetically.dart';
-import 'package:formigas_flutter_lints/lints/sorting/sort_function_and_method_declaration_parameters_alphabetically.dart';
+
+import 'lints/sorting/sort_function_declaration_parameters_alphabetically.dart';
 
 PluginBase createPlugin() => FormigasLint();
 
